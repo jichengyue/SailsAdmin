b7ab21b1a36e0bd4355e1cf8dda5bf4e82886436
jcy <jichengyue@14cells.com>
2015-11-18 16:05:05 +0800

    1、建立adapter 2、联系generator

---
+++ api/controllers/SchoolController.js
@@ -0,0 +1,11 @@
+/**
+ * SchoolController
+ *
+ * @description :: Server-side logic for managing Schools
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/TeacherController.js
@@ -0,0 +1,11 @@
+/**
+ * TeacherController
+ *
+ * @description :: Server-side logic for managing Teachers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
